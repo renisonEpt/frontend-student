a34636f26536fd4fe3110337ad1d2811215e5cfc
Han Chen <han.chen@collectivehealth.com>
2016-04-12 09:55:03 -0700

    added timer functionality, with tech debt

--- src/features/test/index.js
+++ src/features/test/index.js
@@ -4,8 +4,9 @@ import uirouter from 'angular-ui-router';
 import routing from './test.routes';
 import TestController from './test.controller';
 import TestService from "TestService";
-import ngCookies from "angular-cookies"
-export default angular.module('app.test', [uirouter,TestService,ngCookies])
+import ngCookies from "angular-cookies";
+import '../../directives/timer/angular-timer.min.js';
+export default angular.module('app.test', [uirouter,TestService,ngCookies,'timer'])
   .config(routing)
   .controller('TestController', TestController)
   .name;
--- src/features/test/test.controller.js
+++ src/features/test/test.controller.js
@@ -2,17 +2,37 @@ import "angular-local-storage";
 import {TestServiceName} from "TestService";
 import ComponentType from "renison-ept-frontend-core/src/constants/component-type";
 import QuestionType from "renison-ept-frontend-core/src/constants/question-type";
+
 TestController.$inject  = ["$rootScope",'$scope', TestServiceName,"$stateParams", "$state","$q","localStorageService","BaseService","$cookies"];
+
+class Timer{
+	constructor($scope){
+		this.$scope = $scope;
+	}
+	start(timeLeft){
+		this.set(timeLeft);
+		this.$scope.$broadcast('timer-start');
+	}
+	set(timeLeft){
+		if(angular.isNumber(timeLeft) && timeLeft > 0){
+			this.$scope.$broadcast('timer-set-countdown',timeLeft);
+		}
+	}
+	stop(){
+		this.$scope.$broadcast('timer-stop');
+	}
+}
 
 export default function TestController($rootScope,$scope, TestService,$stateParams,$state,$q,localStorageService,BaseService,$cookies) {
 	init();
 	$scope.loaded = false;
-	$scope.timeLeft = 0;
+	$scope.timeLeft = 100;
+	var timer = new Timer($scope);
 
 	function init(){
 		BaseService.get("/proctor/timer").then(function(data){
 			console.log(data);
-			var timeLeft = data.timeLeft;
+			var timeLeft = data.timeLeft; //TODO refactor time left to be passed in data
 			if(timeLeft > 0){
 				$scope.timeLeft = timeLeft;
 				return BaseService.get("/proctor/currentCategory");
@@ -43,10 +63,15 @@ export default function TestController($rootScope,$scope, TestService,$statePara
 
 	function displayTest(){
 		$scope.loaded = true;
-		startTimer();
+		timer.start($scope.timeLeft);
 	}
 
-	function startTimer(){
+	function goToNext(){
+		
+	}
+
+	$scope.onTimerFinished = function(){
 		//TODO
+		console.log("timer finished");
 	}
  }
